87ee3779b10dc74e96f58600999fc939257c9f08
Ajiboye Toluwalase <74120209+metrosmash@users.noreply.github.com>
2026-02-05 19:25:44 +0100

    still on it

--- app.py
+++ app.py
@@ -36,6 +36,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+
 # Get absolute path to project directory
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PKL_DIR = os.path.join(BASE_DIR, 'assets', 'pkl_files')
